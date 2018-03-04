4628f6541b280641970dc4b27cea1f69eb32ef7b
= <jc2535@cornell.edu>
2018-03-03 19:27:03 -0500

    enemy dino

--- core/src/tiktaalik/trino/platform/PlatformController.java
+++ core/src/tiktaalik/trino/platform/PlatformController.java
@@ -38,6 +38,7 @@ public class PlatformController extends WorldController implements ContactListen
 	private static final String DOLL_FILE  = "trino/doll.png";
 	private static final String HERBIVORE_FILE  = "trino/herbivore.png";
 	private static final String CARNIVORE_FILE  = "trino/carnivore.png";
+	private static final String ENEMY_FILE = "trino/enemy.png";
 	/** The texture file for the inedible walls */
 	private static final String WALL_FILE = "trino/wall.png";
 	/** The texture file for the walkable path */
@@ -54,6 +55,8 @@ public class PlatformController extends WorldController implements ContactListen
 
 	/** Texture asset for character avatar */
 	private TextureRegion avatarTexture;
+	/** Texture asset for enemy */
+	private TextureRegion enemyTexture;
 	/** Texture asset for the spinning barrier */
 	private TextureRegion wallTexture;
 	/** Texture asset for the bullet */
@@ -116,6 +119,7 @@ public class PlatformController extends WorldController implements ContactListen
 		}
 		
 		avatarTexture = createTexture(manager,DOLL_FILE,false);
+		enemyTexture = createTexture(manager,ENEMY_FILE, false);
 		wallTexture = createTexture(manager,WALL_FILE,false);
 		pathTexture = createTexture(manager,PATH_FILE,false);
 
@@ -287,7 +291,7 @@ public class PlatformController extends WorldController implements ContactListen
 		dheight = avatarTexture.getRegionHeight()/scale.y;
 		EnemyModel enemy = new EnemyModel(ENEMY_POS.x, ENEMY_POS.y, dwidth, dheight);
 		enemy.setDrawScale(scale);
-		enemy.setTexture(avatarTexture);
+		enemy.setTexture(enemyTexture);
 		addObject(enemy);
 
 		// Create rope bridge
