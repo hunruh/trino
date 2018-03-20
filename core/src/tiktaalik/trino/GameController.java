9b7dfbd3145724f104d6e88f5ea02d9b071740ba
NickTeo96 <nt269@cornell.edu>
2018-03-20 00:34:40 -0400

    duggi on top of exit now

--- core/src/tiktaalik/trino/GameController.java
+++ core/src/tiktaalik/trino/GameController.java
@@ -839,9 +839,9 @@ public class GameController implements ContactListener, Screen {
 		Collections.sort(drawObjects, new Comparator<GameObject>() {
 			@Override
 			public int compare(GameObject g1, GameObject g2) {
-				if (g1.getType() == COTTON || g1.getType() == SWITCH)
+				if (g1.getType() == COTTON || g1.getType() == SWITCH || g1.getType() == GOAL)
 					return -1;
-				if (g2.getType() == COTTON || g2.getType() == SWITCH)
+				if (g2.getType() == COTTON || g2.getType() == SWITCH || g1.getType() == GOAL)
 					return 1;
 				return (int)(g2.getY() - g1.getY());
 			}
