a8dca3d34b1a13ee16b47dbb4c2ae5a8968e8af0
Harrison Unruh <harrison.unruh@gmail.com>
2018-04-06 11:54:29 -0400

    Unnecessary comment

--- core/src/tiktaalik/trino/GameController.java
+++ core/src/tiktaalik/trino/GameController.java
@@ -672,11 +672,6 @@ public class GameController implements ContactListener, Screen {
 		Collections.sort(drawObjects, new Comparator<GameObject>() {
 			@Override
 			public int compare(GameObject g1, GameObject g2) {
-//				if (g1.getType() == GOAL)
-//					return -1;
-//				if (g2.getType() == GOAL)
-//					return 1;
-
 				if (g1.getType() == SWITCH)
 					return -1;
 				if (g2.getType() == SWITCH)
