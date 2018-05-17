04bfd14428ef5284e929f08f04838b92a4113f57
Harrison Unruh <harrison.unruh@gmail.com>
2018-05-16 23:19:37 -0400

    Art fixes

--- core/src/tiktaalik/trino/Level.java
+++ core/src/tiktaalik/trino/Level.java
@@ -589,7 +589,7 @@ public class Level {
                 vineLocation = goalDoor.getGridLocation();
                 goalDoor.setVineTextureSet(filmStripDict.get("vineDrop"),12);
                 goalDoor.setDoorTextureSet(filmStripDict.get("yellowDoor"), 9);
-                goalDoor.setLoweredTextureSet(filmStripDict.get("doorFlashing"), 9);
+                goalDoor.setLoweredTextureSet(filmStripDict.get("doorFlashing"), 16);
             } else if (i == 1) {
                 goalDoor.setGoal(false);
                 goalDoor.setDoorTextureSet(filmStripDict.get("greenDoor"), 9);
